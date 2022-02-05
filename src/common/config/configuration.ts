38364092509bfb36fdfa8043cd0a00edcf23e335
Musah Ibrahim Ali <musahibrahimali@gmail.com>
2022-02-05 00:41:02 +0000

    after a resolve

---
+++ src/common/config/configuration.ts
@@ -0,0 +1,14 @@
+import { config } from "dotenv";
+// configure dotenv
+config();
+
+
+export default () => ({
+    port: parseInt(process.env.PORT, 10) || 5000,
+    domain: process.env.DOMAIN,
+    database: {
+        host: process.env.DB_URL,
+    },    
+    origin: process.env.ORIGIN_URL,
+    uploadsDir: process.env.UPLOADS_DIR,
+});
---
+++ src/common/interface/interface.ts
@@ -0,0 +1,1 @@
+export {IPolicyHandler,PolicyHandler} from './casl.interface';
